f0330cf2fdbbb2e71b5499ade2d879e172c4f605
Jeremy El bez <jeremy.lb@outlook.com>
2018-07-15 23:24:44 -0400

    First draw
    Add basic functionality

---
+++ DotNetWrappers/IO/IIOOperation.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace DotNetWrappers.IO
+{
+    public interface IIOOperation
+    {
+        bool DirectoryExist(string directoryPath);
+        bool FileExist(string filePath);
+        void FileDelete(string filePath);
+
+        Stream CreateFile(string filePath);
+        Stream FileOpenRead(string filePath);
+    }
+}
---
+++ DotNetWrappers/IO/IOOperation.cs
@@ -0,0 +1,33 @@
+﻿
+using System.IO;
+
+namespace DotNetWrappers.IO
+{
+    public class IOOperation : IIOOperation
+    {
+        public Stream CreateFile(string filePath)
+        {
+            return File.Create(filePath);
+        }
+
+        public bool DirectoryExist(string directoryPath)
+        {
+            return Directory.Exists(directoryPath);
+        }
+
+        public void FileDelete(string filePath)
+        {
+            File.Delete(filePath);
+        }
+
+        public bool FileExist(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        public Stream FileOpenRead(string filePath)
+        {
+            return File.OpenRead(filePath);
+        }
+    }
+}
